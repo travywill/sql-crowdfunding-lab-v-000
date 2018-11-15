5a55a530e44699df191a0115a2e5c3b4fbfd82ff
Travis Williamson <travywill@gmail.com>
2018-11-15 02:56:07 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -31,7 +31,11 @@ GROUP BY projects.title HAVING SUM(pledges.amount) - projects.funding_goal >= 0;
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"SELECT users.name, users.age, SUM(pledges.amount)
+FROM users
+INNER JOIN pledges
+ON users.id = pledges.user_id
+GROUP BY users.name;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
