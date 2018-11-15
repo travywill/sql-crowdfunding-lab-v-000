d0ca4e0e946637b4faad745cddd46b8ba7fb1ca3
Travis Williamson <travywill@gmail.com>
2018-11-15 02:37:00 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,11 @@ GROUP BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+"SELECT projects.title, SUM(pledges.amount)
+FROM projects
+INNER JOIN pledges
+ON projects.id = pledges.project_id
+GROUP BY projects.title;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
